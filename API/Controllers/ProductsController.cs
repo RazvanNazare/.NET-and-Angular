a717c7f2340e3263334dec6b89f1cbb2753fbd0d
RazvanNazare <58268118+RazvanNazare@users.noreply.github.com>
2021-04-22 11:43:22 +0300

    Creating the additional projects, Infrastructure and Core class libraries and fixing some things at async part

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,10 +1,16 @@
-using API.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
     [ApiController]
     [Route("api/[controller]")]
+    
     public class ProductsController : ControllerBase
     {
         private readonly StoreContext _context;
@@ -15,14 +21,17 @@ namespace API
         }
 
         [HttpGet]
-        public string GetProducts()
+        public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            return "This will be a list of produc";
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
         }
+
         [HttpGet("{id}")]
-        public string GetProduct(int id)
+        public async Task <ActionResult<Product>> GetProduct(int id)
         {
-            return "single product";
+            return await _context.Products.FindAsync(id);
         }
     }
 }
